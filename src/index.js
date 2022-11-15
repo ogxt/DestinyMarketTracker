298252e7fd417fcc0f5ebc59372fd14dee2634a0
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2022-11-14 21:59:20 -0600

    Initial tooltip

--- src/index.js
+++ src/index.js
@@ -341,3 +341,80 @@ const addTickerEventListener = () => {
 }
 
 // #endregion
+
+// #region Tooltip
+
+const container = document.querySelector(".tracker")
+
+const toolTipWidth = 80;
+const toolTipHeight = 80;
+const toolTipMargin = 15;
+
+// Create and style the tooltip html element
+const toolTip = document.createElement('div');
+toolTip.style = `width: 96px; height: 80px; position: absolute; display: none; padding: 8px; box-sizing: border-box; font-size: 12px; text-align: left; z-index: 1000; top: 12px; left: 12px; pointer-events: none; border: 1px solid; border-radius: 2px;font-family: 'Trebuchet MS', Roboto, Ubuntu, sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;`;
+toolTip.style.background = 'white';
+toolTip.style.color = 'black';
+toolTip.style.borderColor = '#2962FF';
+container.appendChild(toolTip);
+
+chart.subscribeCrosshairMove(param => {
+	if (
+		param.point === undefined ||
+		!param.time ||
+		param.point.x < 0 ||
+		param.point.x > container.clientWidth ||
+		param.point.y < 0 ||
+		param.point.y > container.clientHeight
+	) {
+		toolTip.style.display = 'none';
+	} else {
+		toolTip.style.display = 'block';
+
+        // Finding nearest ticker
+        let nearestSeries = {series: null, distance: -1};
+        param.seriesPrices.forEach((value, series) => {
+            const distance = Math.abs(param.point.y - series.priceToCoordinate(value));
+            if (nearestSeries.distance == -1 || distance < nearestSeries.distance)
+            {
+                nearestSeries = {series: series, distance: distance};
+            }
+        });
+        if (nearestSeries.series == null)
+        {
+            return;
+        }
+
+        const series = nearestSeries.series;
+        const price = param.seriesPrices.get(series);
+
+		toolTip.innerHTML = `<div style="color: ${'#2962FF'}">${series.options().title}</div><div style="font-size: 24px; margin: 4px 0px; color: ${'black'}">
+			${Math.round(100 * price) / 100}
+			</div><div style="color: ${'black'}">
+			</div>`;
+
+		const coordinate = series.priceToCoordinate(price);
+		let shiftedCoordinate = param.point.x + 100;
+		if (coordinate === null) {
+			return;
+		}
+		shiftedCoordinate = Math.max(
+			0,
+			Math.min(container.clientWidth - toolTipWidth, shiftedCoordinate)
+		);
+		const coordinateY =
+			coordinate - toolTipHeight - toolTipMargin > 0
+				? coordinate - toolTipHeight - toolTipMargin
+				: Math.max(
+					0,
+					Math.min(
+						container.clientHeight - toolTipHeight - toolTipMargin,
+						coordinate + toolTipMargin
+					)
+				);
+		toolTip.style.left = shiftedCoordinate + 'px';
+		toolTip.style.top = coordinateY + 'px';
+	}
+});
+
+// #endregion
